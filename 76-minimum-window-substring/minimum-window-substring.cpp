85262d8276a50720258237aff58b943651313368
nainsiii_ <nainsi.a15@gmail.com>
2025-10-27 22:36:13 +0530

    Time: 0 ms (100.00%) | Memory: 11.8 MB (14.45%) - LeetSync

---
+++ 76-minimum-window-substring/minimum-window-substring.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    string minWindow(string s, string t) {
+        int minLen = INT_MAX;
+int sIndex = -1;
+int hash[256] = {0};
+
+for (char c : t) {
+    hash[c]++;
+}
+
+int count = 0;
+int l = 0, r = 0;
+
+while (r < s.size()) {
+    if (hash[s[r]] > 0) {
+        count++;
+    }
+    hash[s[r]]--;
+
+    while (count == t.size()) {
+        if (r - l + 1 < minLen) {
+            minLen = r - l + 1;
+            sIndex = l;
+        }
+
+        hash[s[l]]++;
+        if (hash[s[l]] > 0) {
+            count--;
+        }
+        l++;
+    }
+    r++;
+}
+
+return (sIndex == -1) ? "" : s.substr(sIndex, minLen);
+
+    }
+};
