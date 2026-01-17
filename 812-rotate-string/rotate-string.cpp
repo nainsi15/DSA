2cbbef71d2a61d5485ffbf45ccc51ebaa9677b22
nainsiii_ <nainsi.a15@gmail.com>
2026-01-17 15:15:36 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (61.32%) - LeetSync

--- 812-rotate-string/rotate-string.cpp
+++ 812-rotate-string/rotate-string.cpp
@@ -1,14 +1,12 @@
 class Solution {
 public:
     bool rotateString(string s, string goal) {
+			// Strings must be same length to be rotations of each other
 			if(s.length() != goal.length()){
 				return false;
 			}
-			for(int i = 0  ;i  < s.length(); i++){
-				string right = s.substr(i);
-				string left = s.substr(0, i);
-				if(right + left == goal) return true;
-			}
-			return false;
+			string doubleS = s + s;  // Concatenate s with itself
+			//Check if goal is a substring of s + s
+			return doubleS.find(goal) != string::npos;
     }
 };
