9ae2ba6f224e3f2b3d5f9ed004bba602a0650526
nainsiii_ <nainsi.a15@gmail.com>
2026-02-12 23:23:23 +0530

    Time: 0 ms (100.00%) | Memory: 12 MB (29.39%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.cpp
+++ 14-longest-common-prefix/longest-common-prefix.cpp
@@ -1,26 +1,21 @@
 class Solution {
 public:
     string longestCommonPrefix(vector<string>& strs) {
-        //if emty string 
-			if(strs.empty()) return "";
+        int n = strs.size();
+        sort (strs.begin(), strs.end());
+        string ans = "";
+        string first = strs[0];
+        string last = strs[n-1];
 
-			int n = strs.size();
-			// Sort the vector to get the lexicographically smallest and largest strings
-			sort(strs.begin(), strs.end());
-
-			//first string
-			string first = strs[0];
-			//last string
-			string last = strs[n-1];
-			string ans = "";
-			// Compare characters of the first and last strings
-			int minlength = min(first.size(), last.size());
-			for(int i = 0 ; i < minlength ; i++){
-				if(first[i] != last[i]){
-				return ans;
-				}
-				ans += first[i];
-			}
-				return ans;
+        int min_length = min(first.size(), last.size());
+        for(int i = 0 ; i < min_length ; i++){
+            if(first[i] != last[i]){
+                return ans;
+            }
+            else{
+               ans.push_back(first[i]);
+            }
+        }
+        return ans;
     }
 };
