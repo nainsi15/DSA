15610c87ab3a2befa13fd11f3853a40c0b63aa7a
nainsiii_ <nainsi.a15@gmail.com>
2026-05-22 23:57:05 +0530

    Time: 39 ms (63.16%) | Memory: 62.6 MB (56.70%) - LeetSync

--- 77-combinations/combinations.cpp
+++ 77-combinations/combinations.cpp
@@ -1,17 +1,21 @@
 class Solution {
 private:
     void func(int ind, int n, int k, vector<int> &nums, vector<int> &arr,vector<vector<int>> &ans){
+        //for out of bound conditions
+        //if got only elements < k 
+        //just return 
+        //if equal to k then push back
         if(ind == n){
         if(arr.size() == k){
             ans.push_back(arr);
         }
             return;
         }
-
+        //to pick the elements
         arr.push_back(nums[ind]);
         func(ind+1, n, k, nums, arr, ans);
         arr.pop_back();
-
+        //not picking the element
         func(ind+1, n, k, nums, arr, ans);
     }
 public:
@@ -19,6 +23,7 @@ public:
         vector<int> arr;
         vector<vector<int>> ans;
         vector<int> nums;
+        //pushing the required 'n' elemnts to the nums
         for(int i = 1 ; i <= n ; i++){
             nums.push_back(i);
         }
