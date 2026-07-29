dd1debc0efb51a15ec1de25fd7362455d945900e
nainsiii_ <nainsi.a15@gmail.com>
2026-07-29 11:35:19 +0530

    Time: 47 ms (64.34%) | Memory: 29 MB (73.61%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -4,31 +4,29 @@ public:
         int n = nums.size();
         sort(nums.begin(), nums.end());
         vector<vector<int>> ans;
-        
+
         for(int i = 0 ; i < n; i++){
             if(i > 0 && nums[i] == nums[i-1]) continue;
             int j = i+1;
-            int k = n-1;
+            int k =  n-1;
 
             while(j < k){
-            int sum =  nums[i] + nums[j] +nums[k];
+                int sum = nums[i] + nums[j] + nums[k];
+                if(sum < 0) j++;
+                else if( sum > 0) k--;
+                else{
+                    ans.push_back({nums[i], nums[j] , nums[k]});
+                    j++;
+                    k--;
+                
+                  while(j < k && nums[j] == nums[j-1])
+                        j++;
 
-            if(sum < 0){
-                j++;
+                    while(j < k && nums[k] == nums[k+1])
+                        k--;
+                }
             }
-            else if (sum > 0){
-                k--;
-            }
-            else{
-                ans.push_back({nums[i], nums[j],nums[k]});
-                j++;
-                k--;
-                while(j < k && nums[j] == nums[j-1]) j++;
-                while(k > 0 && nums[k] == nums[k+1]) k--;
-            }
-        }
         }
         return ans;
-
     }
 };
